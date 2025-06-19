0d2c0990ac95bd5ba5749b9ac6bf435b8acf7f8a
Perkiins <erxuulo@gmail.com>
2025-06-19 18:27:02 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Build script para Render
+
+echo "Instalando permisos para chromedriver..."
+chmod +x /usr/lib/chromium-browser/chromedriver || true
+
+echo "Configuración de entorno completada."
